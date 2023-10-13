691af6aefaea7ccddf32744c9c06230b34d2ab3b
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-10-12 22:30:23 -0400

    shopping cart looks good

--- client/src/Components/Pieces/ShoppingCartModal.js
+++ client/src/Components/Pieces/ShoppingCartModal.js
@@ -35,14 +35,16 @@ function ShoppingCartModal ({onClose}) {
             </div> */}
             <div className="bg-white p-2 rounded-xl ">
                 <div className="text-black text-xl  cursor-pointer flex justify-between" onClick={() => onClose()}>
-                    <h2 className = "mb-2">Shopping Cart:</h2>
+                    <h2 className = "mb-2 ml-2">Shopping Cart:</h2>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     
                 </div>
-            {/* <h2 className = "mb-2">Shopping Cart:</h2> */}
+            <div className="max-h-[400px] overflow-y-auto">
                 {productsInCart.map((product)=> (
+                    <div>
+                        <hr class="h-px my-1 bg-gray-200 border-1 light:bg-gray-200"></hr>
                     <div key={product._id} className="flex gap-3 py-1 px-3">
                         <div className = " h-[100px] w-[100px]">
                             <img src={product.image} atl={product.title} />
@@ -58,12 +60,15 @@ function ShoppingCartModal ({onClose}) {
                             </div>
                         </div>
                     </div>
+                    </div>
                 ))}
-                <div className = "flex in-line justify-between mr-3">
-                    <p className = "mt-1 ml-3 ">Total Price:${addTotalCost()}</p>
-                    <button className = " bg-emerald-200 p-1 px-5 mb-2 rounded-xl shadow hover:shadow-xl hover:bg-emerald-400">
+                </div>
+                
+                <div className = "flex in-line justify-between mr-3 mt-3 ">
+                    <button className = "bg-emerald-200 p-1 px-5 mb-2 ml-2 rounded-xl shadow hover:shadow-xl hover:bg-emerald-400">
                         Place Order
                     </button>
+                    <p className = "mt-1 ml-3 font-bold text-xl">Total Price: ${addTotalCost()}</p>
                 </div>
             </div>
         </div>
